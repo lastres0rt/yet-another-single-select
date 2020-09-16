de1d792ea33b65e6e8a04ed1d71d25365c64073c
Rachel Keslensky <rach@lid.cc>
2020-09-16 00:01:46 -0700

    Create gh-pages branch with static content

---
+++ precache-manifest.221c38764e1cd7ef1b60dbac15aee273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a88b1f27d84c0ed08f44b118b181023",
+    "url": "/yet-another-single-select/index.html"
+  },
+  {
+    "revision": "5a157e98eaae9abc3e34",
+    "url": "/yet-another-single-select/static/css/main.7f071b5f.chunk.css"
+  },
+  {
+    "revision": "b4122b91560cb42ef1fa",
+    "url": "/yet-another-single-select/static/js/2.6c313b41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yet-another-single-select/static/js/2.6c313b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a157e98eaae9abc3e34",
+    "url": "/yet-another-single-select/static/js/main.cda59b50.chunk.js"
+  },
+  {
+    "revision": "a15937328de2e1289e7b",
+    "url": "/yet-another-single-select/static/js/runtime-main.a3f3a90c.js"
+  }
+]);
